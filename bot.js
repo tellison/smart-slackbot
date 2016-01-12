7cbccc1c149c64654b33cb66d7c6429136ed9b9b
Tim Ellison <t.p.ellison@gmail.com>
2016-01-12 13:59:53 +0000

    Use express as a simple webserver just to show willing.

--- bot.js
+++ bot.js
@@ -1,3 +1,15 @@
+var express = require('express');
+var cfenv = require('cfenv');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+var appEnv = cfenv.getAppEnv();
+app.listen(appEnv.port, '0.0.0.0', function() {
+  console.log("server starting on " + appEnv.url);
+});
+
+
+
+
 var Botkit = require('botkit')
 var os = require('os');
 
